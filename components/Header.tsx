16f34c2691b26aae8ce22a310b15cd9a2fa98134
mykykh <49101849+mykykh@users.noreply.github.com>
2023-07-15 23:10:47 +0300

    fix: fix header positioning

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,13 @@ import ConnectButton from "./ConnectButton";
 
 export default function Header() {
     return (
-        <header className="flex p-2 justify-between align-center bg-secondary">
-            <h2 className="text-2xl">drtr</h2>
-            <ConnectButton />
-            <div></div>
+        <header className="sticky top-0 grid grid-cols-3 p-2 bg-secondary">
+            <div className="flex">
+                <h2 className="text-2xl">drtr</h2>
+            </div>
+            <div className="flex justify-center">
+                <ConnectButton />
+            </div>
         </header>
     )
 }
