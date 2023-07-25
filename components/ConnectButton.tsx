6ce042a12c03e2074aed7a3f60b073f046857930
mykykh <49101849+mykykh@users.noreply.github.com>
2023-07-25 11:12:23 +0300

    feat: add wallet auto connect

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -1,18 +1,37 @@
 "use client"
 import { useMoralis } from "react-moralis"
 import PrimaryButton from "./PrimaryButton"
+import { useEffect } from "react"
 
 export default function ConnectButton() {
-    const {enableWeb3, account, deactivateWeb3} = useMoralis()
+    const {isWeb3Enabled, enableWeb3, account, deactivateWeb3} = useMoralis()
+    useEffect(() => {
+        if (!isWeb3Enabled) {
+            if (window) {
+                if (window.localStorage.getItem("connected")) {
+                    enableWeb3()
+                }
+            }
+        }
+    }, [isWeb3Enabled])
     return (
         <div>
            {account ? 
                 <PrimaryButton
                 text={`${account.slice(0, 6)}...${account.slice(account.length - 4)}`} 
-                onClickHandler={() => deactivateWeb3()} /> :
+                onClickHandler={() => {deactivateWeb3()
+                        if (window) {
+                            window.localStorage.removeItem("connected")
+                        }
+                    }} /> :
                 <PrimaryButton
                 text="Connect wallet" 
-                onClickHandler={() => enableWeb3()} />}
+                onClickHandler={() => {
+                        enableWeb3()
+                        if (window) {
+                            window.localStorage.setItem("connected", "injected")
+                        }
+                    }} />}
         </div>
     )
 }
